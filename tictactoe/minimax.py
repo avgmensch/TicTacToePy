0e625326aa798e2b7b812cc7adffaf41dc120492
avgmensch <124395199+avgmensch@users.noreply.github.com>
2024-03-09 22:51:07 +0100

    improve minimax, implement minimax in gameloop

--- tictactoe/minimax.py
+++ tictactoe/minimax.py
@@ -1,22 +1,24 @@
 from .constants import PLAYER, COMPUTER, Tile, Board
-from .helper import ttt_get_available_moves, ttt_check_winner
+from .helper import ttt_get_available_moves, ttt_check_winner, ttt_get_opposite_turn
 
 
 def minimax(board: Board, turn: Tile, depth: int = 0) -> int:
+    # TODO: Use depth
     if turn != PLAYER and turn != COMPUTER:
         raise Exception()  # TODO: Create actual exception
 
     winner = ttt_check_winner(board)
     if winner == PLAYER:
-        return -10
+        return depth - 10
     elif winner == COMPUTER:
-        return 10
+        return 10 - depth
     elif winner is None:
         return 0
 
     # [(x, y), (x, y), ...]
     available_moves = ttt_get_available_moves(board)
 
+    # Minimizer
     if turn == PLAYER:
         best_score = 99
         for x, y in available_moves:
@@ -27,6 +29,7 @@ def minimax(board: Board, turn: Tile, depth: int = 0) -> int:
             board[y][x] = old
         return best_score
 
+    # Maximizer
     elif turn == COMPUTER:
         best_score = -99
         for x, y in available_moves:
@@ -39,3 +42,28 @@ def minimax(board: Board, turn: Tile, depth: int = 0) -> int:
 
     else:
         raise Exception()  # TODO: Create actual exception
+
+
+def ttt_get_best_move(board: Board, turn: Tile) -> tuple[int, int]:
+    """Returns (x, y)"""
+    if turn != PLAYER and turn != COMPUTER:
+        raise Exception()  # TODO: Create actual exception
+
+    best_move: tuple[int, int] = (0, 0)
+    best_score: int = 99 if turn == PLAYER else -99
+    available_moves = ttt_get_available_moves(board)
+
+    for x, y in available_moves:
+        old = board[y][x]
+        board[y][x] = turn
+        s = minimax(board, ttt_get_opposite_turn(turn))
+        board[y][x] = old
+        print(s)
+        if (
+            (turn == PLAYER and s < best_score) or
+            (turn == COMPUTER and s > best_score)
+        ):
+            best_score = s
+            best_move = (x, y)
+
+    return best_move
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,8 +1,9 @@
-from .constants import PLAYER, Tile
+from .constants import COMPUTER, PLAYER, Tile
 from .exceptions import Invalid2dIndexException, \
     TileAtIndexAlreadyOccupiedException
-from .helper import ttt_get_valid_1d_index, ttt_generate_board, \
+from .helper import ttt_check_winner, ttt_get_valid_1d_index, ttt_generate_board, \
     ttt_print_board, ttt_validate_tile_place_attempt, ttt_get_opposite_turn, ttt_convert_1d_to_2d
+from .minimax import ttt_get_best_move
 
 
 def ttt_game() -> tuple[int, int]:
@@ -13,26 +14,39 @@ def ttt_game() -> tuple[int, int]:
     board = ttt_generate_board()
     turn: Tile = PLAYER
     while True:
-        ttt_print_board(board)
-        error_message: str = ""
-        while True:
-            try:
-                if error_message != "":
-                    print(error_message)
-                i1d = ttt_get_valid_1d_index(f"{turn}>")
-                if i1d == -1:
-                    return (0, 0)
-                ix2d, iy2d = ttt_convert_1d_to_2d(i1d)
-                ttt_validate_tile_place_attempt(board, ix2d, iy2d)
-                board[iy2d][ix2d] = turn
-            except Invalid2dIndexException:
-                error_message = "This position does not exist!"
-                continue
-            except TileAtIndexAlreadyOccupiedException:
-                error_message = "This position is already occupied!"
-                continue
-            except Exception as e:
-                raise e
-            break
+        if turn == PLAYER:
+            ttt_print_board(board)
+            error_message: str = ""
+            while True:
+                try:
+                    if error_message != "":
+                        print(error_message)
+                    i1d = ttt_get_valid_1d_index(f"{turn}>")
+                    if i1d == -1:
+                        return (0, 0)
+                    ix2d, iy2d = ttt_convert_1d_to_2d(i1d)
+                    ttt_validate_tile_place_attempt(board, ix2d, iy2d)
+                    board[iy2d][ix2d] = turn
+                except Invalid2dIndexException:
+                    error_message = "This position does not exist!"
+                    continue
+                except TileAtIndexAlreadyOccupiedException:
+                    error_message = "This position is already occupied!"
+                    continue
+                except Exception as e:
+                    raise e
+                break
+        elif turn == COMPUTER:
+            x, y = ttt_get_best_move(board, COMPUTER)
+            board[y][x] = COMPUTER
+        else:
+            raise Exception()  # TODO: Create actual exception
+        winner = ttt_check_winner(board)
+        if winner == PLAYER:
+            return (1, 0)
+        elif winner == COMPUTER:
+            return (0, 1)
+        elif winner is None:
+            return (0, 0)
         turn = ttt_get_opposite_turn(turn)
         print()
